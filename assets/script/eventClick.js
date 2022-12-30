1b3b90beaf0d99eae79aefd65e63470ca49469fb
Sergio <sergiohrodriguess@gmail.com>
2022-12-30 17:07:28 -0300

    card limpando ao iniciar

--- assets/script/eventClick.js
+++ assets/script/eventClick.js
@@ -4,6 +4,8 @@ const formActive  = document.querySelector('.form-sucess');
 let btnContinue = document.querySelector('.input-continue');
 const inpts = document.querySelectorAll('.required');
 
+const campoCartao = document.querySelectorAll('.campoCartao')
+
 import { setError } from "./validandoCampos.js";
 import { removeError } from "./validandoCampos.js";
 
@@ -14,18 +16,27 @@ btnConfirm.addEventListener('click', (e) => {
     for(let i = 0; i < inpts.length; i++){
         if(inpts[i].value === ""){
             setError(i)
-        } else {
-            removeError(i);
+        } else{
             formDisable.style.display = 'none'
             formActive.style.display = 'block'
+            removeError(i);
         }
     }
-    
 })
 
 btnContinue.addEventListener('click', () => {
     inpts.forEach(inpt => inpt.value = "")
 
+    campoCartao[0].innerHTML = "0000 0000 0000 0000"
+    campoCartao[1].innerHTML = "JANE APPLESEED"
+    campoCartao[2].innerHTML = "00/00"
+    campoCartao[3].innerHTML = "000"
+
     formDisable.style.display = 'block'
     formActive.style.display = 'none'
+    
+
+    for (let i = 0; i < inpts.length; i++){
+        removeError(i);
+    }
 })
