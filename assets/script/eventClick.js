c2ec7aa1ae8ac5661bdba6a45308c7e8b656ebf0
Sergio <sergiohrodriguess@gmail.com>
2022-12-30 17:10:16 -0300

    let por const

--- assets/script/eventClick.js
+++ assets/script/eventClick.js
@@ -1,10 +1,10 @@
 const btnConfirm  = document.querySelector('.input-button');
 const formDisable = document.querySelector('#form');
 const formActive  = document.querySelector('.form-sucess');
-let btnContinue = document.querySelector('.input-continue');
+const btnContinue = document.querySelector('.input-continue');
 const inpts = document.querySelectorAll('.required');
-
 const campoCartao = document.querySelectorAll('.campoCartao')
+
 
 import { setError } from "./validandoCampos.js";
 import { removeError } from "./validandoCampos.js";
